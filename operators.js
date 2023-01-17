044599c8bc157f6c3e1bf082b118194d88529933
Munirat123 <109763823+Munirat123@users.noreply.github.com>
2023-01-17 22:32:10 +0100

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,75 @@
+// Two let variables num1 and num2
+
+let num1 = 10;
+let num2 = 5;
+
+// Perform basic mathematical operations on these variables
+
+// addition
+console.log('num1 + num2= ', num1 + num2);  // 15
+
+// subtraction
+console.log('num1 - num2= ', num1 - num2);  // 5
+
+// multiplication
+console.log('num1 * num2= ', num1 * num2);  // 50
+
+// division
+console.log('num1 / num2= ', num1 / num2);  // 2
+
+// modulus
+console.log('num1 % num2= ', num1 % num2);   // 0
+
+// Use the increment (++) and decrement (--) operators
+
+// increment
+console.log('++num1 = ', ++num1); // num1 is now 11
+console.log('num1++ = ', num1++); // prints 11 and then it increased to 12
+console.log('num1 = ', num1);     // 12
+
+console.log('++num2 = ', ++num2); // num2 is now 6
+console.log('num2++ = ', num2++); // prints 6 and then it increased to 7
+console.log('num2 = ', num2);     // 7
+
+// decrement
+console.log('--x = ', --num2); // num1 is now 11
+console.log('num1-- = ', num1--); // prints 11 and then it decreased to 10
+console.log('num1 = ', num1);     // 10
+
+console.log('--x = ', --num1); // num2 is now 6
+console.log('num2-- = ', num2--); // prints 6 and then it decreased to 5
+console.log('num2 = ', num2);     // 5
+
+// Grab user input with prompt function
+
+var num1 = readInt("Input a number");
+var num2 = readInt("Input a number");
+
+println("Hello " + num1 + "!");
+println(num + "?! That's one of my favorite numbers!");
+
+println("Hello " + num2 + "!");
+println(num + "?! That's one of my favorite numbers!");
+
+// Try explicit conversion
+
+parseFloat(num1);
+parseFloat(num2);
+
+// Arithmetic operators with the new values
+
+// addition
+console.log('num1 + num2= ', num1 + num2);  
+
+// subtraction
+console.log('num1 - num2= ', num1 - num2);  
+
+// multiplication
+console.log('num1 * num2= ', num1 * num2);  
+
+// division
+console.log('num1 / num2= ', num1 / num2);  
+
+// modulus
+console.log('num1 % num2= ', num1 % num2);  
+
